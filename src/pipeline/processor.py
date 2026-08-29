2b5d3ff23cdaf580369829b00a88cc51d33550ed
Faith Ogundusi <ogundusifaith.1@gmail.com>
2026-08-29 13:25:59 +0100

    Add sales data processing module

---
+++ src/pipeline/processor.py
@@ -0,0 +1,15 @@
+"""
+Data processing module for the AWS Sales Data Pipeline V2.
+
+This module will contain the transformation and processing
+logic used after sales records are received.
+"""
+
+
+def process_record(record):
+    """
+    Process a validated sales record.
+
+    Transformation logic will be implemented here.
+    """
+    pass
