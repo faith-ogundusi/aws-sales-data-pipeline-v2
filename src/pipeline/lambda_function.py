69587e1419ceb6efb0d35c408e264d512cfb9fb7
Faith Ogundusi <ogundusifaith.1@gmail.com>
2026-08-29 13:23:38 +0100

    Add Lambda project entry point

---
+++ src/pipeline/lambda_function.py
@@ -0,0 +1,11 @@
+"""
+AWS Sales Data Pipeline V2
+
+Main AWS Lambda entry point.
+
+This module will handle:
+- S3 event processing
+- sales data validation
+- data transformation
+- valid/rejected record routing
+"""
